bef9da96ba2c3883cbe68c78ec05a617081a0511
taishang <nttyz2004@sina.com>
2020-11-22 12:42:06 +0800

    new file:   kernel/mm/Page.h

---
+++ kernel/mm/Page.h
@@ -0,0 +1,17 @@
+//SystemRuiProject
+//祈瑞
+//惊鸿回眸动琴心，倩兮巧笑近有情
+//白雁难托寄红豆，青鸾无语赠紫荆
+//清婉无需羡周南，溯洄未敢效长卿
+//远望山林祈瑞瑷，君不念我仍思君
+//@author:taishang
+//@email:tuyanzheng@gmail.com
+struct cr3{
+	unsigned int reserved1: 3;
+	unsigned int PWT: 1;
+	unsigned int PCD: 1;
+	unsigned int reserved2: 7;
+	unsigned int Base: 52;
+};
+
+
